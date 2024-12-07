17243ef7eb071eeaa76c31799a98e83b5698e835
Ronaldo Cesar Torre <ronaldo.torre@econective.com.br>
2024-12-07 17:49:57 -0300

    Add module tenant

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { PartnersModule } from './partners/partners.module';
 import { CoursesModule } from './courses/courses.module';
+import { TenantModule } from './tenant/tenant.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, PartnersModule, CoursesModule],
+  imports: [PrismaModule, AuthModule, PartnersModule, CoursesModule, TenantModule],
   controllers: [AppController],
   providers: [AppService],
 })
